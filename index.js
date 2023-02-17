96b3a25e7904d88d0e81b478720baf61fc8b30b2
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-17 16:08:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const port = process.env.PORT || 8080;
+
+const jwtCheck = auth({
+  audience: 'https://harshan-dev-92.com/api',
+  issuerBaseURL: 'https://dev-oc4r10h8h01y4zn6.us.auth0.com/',
+  tokenSigningAlg: 'RS256'
+});
+
+app.use(cors());
+// enforce on all endpoints
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
+
+app.listen(port);
+
+console.log('Running on port ', port);
